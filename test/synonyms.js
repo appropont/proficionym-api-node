6ab0e73c2f0203be2d00fba11e3db6f8cdecced7
Chris Griffing <cmgriffing@gmail.com>
2015-02-16 09:01:31 -0800

    fixed test saffolding since browser tests are unnecessary

--- test/synonyms.js
+++ test/synonyms.js
@@ -4,7 +4,7 @@ var should = require('chai').should(); //actually call the function
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      ([1,2,3].indexOf(5)).should.equal(-1);
+      (-1).should.equal(-2);
     })
   })
 })
