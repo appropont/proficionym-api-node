a6d8b5f1d24abb86e727be30b5eb198e108b21d7
Chris Griffing <cmgriffing@gmail.com>
2015-02-16 14:02:14 -0800

    added tests for domains.isDomainAvailable

--- test/domains.js
+++ test/domains.js
@@ -7,8 +7,56 @@ describe('Domains', function() {
         should.exist(domains);
     });
 
-    it('should have the core method (isDomainAvailable)', function() {
-    	should.exist(domains.isDomainAvailable);
-    });
+});
+
+describe('Domains.isDomainAvailable', function() {
+
+	it('should be defined', function() {
+		should.exist(domains.isDomainAvailable);
+	});
+
+	it('should resolve "google.com" with a status of registered', function(done) {
+		domains.isDomainAvailable('google.com')
+			.then(function(result) {
+				should.exist(result);
+				result.should.have.property('domain', 'google.com');
+				result.should.have.property('status', 'registered');
+				done();
+			})
+			.error(function(error) {
+				should.not.exist(error);
+				done();
+			});
+	});
+
+	it('should resolve "google.google" with a status of error', function(done) {
+		domains.isDomainAvailable('google.google')
+			.then(function(result) {
+				should.exist(result);
+				result.should.have.property('domain', 'google.google');
+				result.should.have.property('status', 'error');
+				done();
+			})
+			.error(function(error) {
+				should.not.exist(error);
+				done();
+			});
+	});
+
+
+
+	it('should resolve "test123test789.com" with a status of available', function(done) {
+		domains.isDomainAvailable('test123test789.com')
+			.then(function(result) {
+				should.exist(result);
+				result.should.have.property('domain', 'test123test789.com');
+				result.should.have.property('status', 'available');
+				done();
+			})
+			.error(function(error) {
+				should.not.exist(error);
+				done();
+			});
+	});
 
 });
