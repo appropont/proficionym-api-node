192fbb5e1b11e1256ab9f36b0a07752e3c83fdf7
Chris Griffing <cmgriffing@gmail.com>
2015-02-16 13:46:17 -0800

    rewrote assertions to be more in line with BDD

--- test/synonyms.js
+++ test/synonyms.js
@@ -4,11 +4,7 @@ var should = require('chai').should(),
 describe('Synonyms', function() {
 
     it('should be defined', function() {
-        synonyms.should.not.equal(undefined);
-    });
-
-    it('should have the core method (getSynonyms)', function() {
-    	synonyms.getSynonyms.should.not.equal(undefined);
+        should.exist(synonyms);
     });
 
 });
@@ -17,36 +13,36 @@ describe('Synonyms', function() {
 describe('Synonyms._extractKeywords', function() {
 
     it('should be defined', function() {
-    	synonyms._extractKeywords.should.not.equal(undefined);
+    	should.exist(synonyms._extractKeywords);
     });
 
     it('should remove whitespace', function() {
     	var keywords = synonyms._extractKeywords('a test string\n');
-    	keywords.length.should.equal(1);
+    	keywords.should.have.length(1);
     	keywords[0].should.equal('ateststring');
     });
 
     it('should remove parentheses and words in them', function() {
     	var keywords = synonyms._extractKeywords('ateststring(testing)');
-    	keywords.length.should.equal(1);
+    	keywords.should.have.length(1);
     	keywords[0].should.equal('ateststring');
     });
 
     it('should remove empty square brackets', function() {
     	var keywords = synonyms._extractKeywords('ateststring[]');
-    	keywords.length.should.equal(1);
+    	keywords.should.have.length(1);
     	keywords[0].should.equal('ateststring');
     });
 
     it('should remove hyphens', function() {
     	var keywords = synonyms._extractKeywords('a-test-string');
-    	keywords.length.should.equal(1);
+    	keywords.should.have.length(1);
     	keywords[0].should.equal('ateststring');
     });
 
     it('should split words on commas', function() {
     	var keywords = synonyms._extractKeywords('a,test,string');
-    	keywords.length.should.equal(3);
+    	keywords.should.have.length(3);
     	keywords[0].should.equal('a');
     	keywords[1].should.equal('test');
     	keywords[2].should.equal('string');
@@ -54,7 +50,7 @@ describe('Synonyms._extractKeywords', function() {
 
     it('should replace semicolons with commas', function() {
     	var keywords = synonyms._extractKeywords('a;test;string');
-    	keywords.length.should.equal(3);
+    	keywords.should.have.length(3);
     	keywords[0].should.equal('a');
     	keywords[1].should.equal('test');
     	keywords[2].should.equal('string');
@@ -65,7 +61,7 @@ describe('Synonyms._extractKeywords', function() {
 describe('Synonyms._parseSynonymsXML', function() {
 
     it('should be defined', function() {
-    	synonyms._parseSynonymsXML.should.not.equal(undefined);
+    	should.exist(synonyms._parseSynonymsXML);
     });
 
     it('should error on non-object', function() {
@@ -98,16 +94,13 @@ describe('Synonyms._parseSynonymsXML', function() {
 
     	var result = synonyms._parseSynonymsXML(validInput);
 
-
-    	console.log('result', result);
-
-    	result.length.should.equal(1);
+    	result.should.have.length(1);
     	result[0].should.have.property('wordType', 'noun');
     	result[0].should.have.property('senses');
-    	result[0].senses.length.should.equal(1);
+    	result[0].senses.should.have.length(1);
     	result[0].senses[0].should.have.property('meaning', 'test meaning');
     	result[0].senses[0].should.have.property('words');
-    	result[0].senses[0].words.length.should.equal(8);
+    	result[0].senses[0].words.should.have.length(8);
 
     });
 
@@ -145,6 +138,6 @@ describe('Synonyms.getSynonyms', function() {
 				should.not.exist(error);
 				done();
 			});
-			
+
 	});
 });
